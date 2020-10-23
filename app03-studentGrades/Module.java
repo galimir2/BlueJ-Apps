17f1c6b0a6ab2d63bd0ff5ba66744271f08036f5
galimir2 <g.bozmarov00@gmail.com>
2020-10-23 11:51:22 +0100

    a little to finish

---
+++ app03-studentGrades/Module.java
@@ -0,0 +1,65 @@
+
+/**
+ * Write a description of class Module here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Module
+{
+    // Fields
+    private String title;
+    
+    private String codeNo;
+    
+    private int mark;
+    
+    private int credit;
+    
+    private boolean passed;
+    
+    private boolean completed;
+    
+    /**
+     * Constructor for objects of class Module
+     */
+    public Module(String title, String codeNo)
+    {
+        this.title = title;
+        this.codeNo = codeNo;
+        
+        mark = -1;
+        credit = 15;
+        
+        passed = false;
+        completed = false;
+    }
+    
+    public void awardMark(int mark)
+    {
+        this.mark = mark;
+    }
+    
+    public int getMark()
+    {
+        return mark;
+    }
+
+    public String getCodeNo()
+    {
+        return codeNo;
+    }
+    
+    /**
+     *  This will return a mark if its greater or equal to 0 when the course is completed.
+     */
+    public boolean isCompleted()
+    {
+        return mark >= 0;
+    }
+    
+    public void print()
+    {
+        System.out.println("Module: " + title + " " + codeNo + " Mark = " + mark);
+    }
+}
