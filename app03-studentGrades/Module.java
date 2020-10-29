360ae9afa3af4c8d31be518646d00b71c0c86e4b
galimir2 <g.bozmarov00@gmail.com>
2020-10-29 22:20:44 +0000

    worked on the modules comments

--- app03-studentGrades/Module.java
+++ app03-studentGrades/Module.java
@@ -2,8 +2,8 @@
 /**
  * Write a description of class Module here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Galimir Bozmarov
+ * @version 1.0
  */
 public class Module
 {
